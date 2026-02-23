ccab586a692b5ca5703d5bb0220d815f44191b62
laona2050 <laona2050@github.com>
2026-02-23 21:41:33 +0800

    修改网站标题

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "老鹏友";
+export const SITE_TITLE = "老鹏友，欢迎你的到来";
 export const SITE_DESCRIPTION = "老鹏友的博客，分享游艇、生活和旅行";
